80ef36d80e89041805e50b93c885968f40240b89
Kevin Schroers <kevinschroers98@gmail.com>
2024-07-22 21:28:14 +0200

    Inventarsystem erneuert

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,10 +2,20 @@
 extends CharacterBody2D
 class_name Player
 
-@export var sprite : AnimatedSprite2D
+@export var inventory_data: InventoryData
+signal toggle_inventory
+
+@onready var sprite = $AnimatedSprite2D
+
+
 
 var last_direction : Vector2
 var SPEED = 300
 
 func _ready():
 	Global.player = self
+
+func _unhandled_input(_event):
+	
+	if Input.is_action_just_pressed("inventory"):
+		toggle_inventory.emit()
